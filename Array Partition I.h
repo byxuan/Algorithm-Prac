027e7301d37da4f1349fbe1b776c86112c20e404
byxuan <byxuan@163.com>
2017-10-31 13:22:06 +0800

    Add files via upload

---
+++ Array Partition I.h
@@ -0,0 +1,29 @@
+#pragma once
+
+/*
+Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) 
+which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+Example:
+Input: [1,4,3,2]
+Output: 4
+Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+
+Analysis:
+if the array is [2,3,1,4,6,5], firstly make the order 1, 2, 3, 4, 5, 6, and what we need is the sum of 1, 3, 5.
+*/
+
+#include <algorithm>						//include sort()
+#include <functional>						//include compare function like greater<type>(), less<type>().
+
+class Solution
+{
+public:
+	int arrayPairSum(int nums[]) {
+		sort(nums,nums+nums.size())			//the order is increase in default
+		int result = 0;
+		for (int i = 0; i < nums.size(); i += 2)
+			result += nums[i];
+	}
+	return result;
+};
